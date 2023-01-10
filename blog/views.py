7a52eaa1be005e1012d661b37bd4d4761b8aa442
birkan <b.561birkan@gmail.com>
2023-01-10 22:42:53 +0300

    serializers, views, endpoints

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
 
 # Create your views here.
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.filter(is_published = True)
+    serializer_class = BlogSerializer
+
 
