be20b8cf71bd939887863768058e7147c464e788
birkan <b.561birkan@gmail.com>
2023-01-10 23:18:51 +0300

    pagination, filter, searchFields

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,13 @@ from .serializers import CategorySerializer, BlogSerializer
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filterset_fields = ["name"]
 
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.filter(is_published = True)
     serializer_class = BlogSerializer
+    filterset_fields = ["category"]
+    search_fields = ["title", "content"]
 
 
--- main/settings.py
+++ main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # third_party
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter'],
+
+}
