44fa47697f878f2a5be743f03805ca997fb9ca73
birkan <b.561birkan@gmail.com>
2023-01-11 13:23:55 +0300

    permissions (Custom permission, IsAuthenticatedOrReadOnly )

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,16 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly  # custom permission
+
 # Create your views here.
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filterset_fields = ["name"]
+    permission_classes = [IsAdminOrReadOnly]  #  permission_classes = [IsAdminUser | ReadOnly]
 
 
 class BlogView(ModelViewSet):
@@ -15,5 +19,6 @@ class BlogView(ModelViewSet):
     serializer_class = BlogSerializer
     filterset_fields = ["category"]
     search_fields = ["title", "content"]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
